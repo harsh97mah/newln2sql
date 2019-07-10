b74bc8f94d662d18e48f18da7acc10164997c992
harsh97mah <50320524+harsh97mah@users.noreply.github.com>
2019-07-10 17:32:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ln2sql',
+    name='newln2sql',
     version='0.2',
     url='https://github.com/FerreroJeremy/ln2sql',
     license='GNU',
