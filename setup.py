6ec4797e824a4424cb696151dba606cd774337b9
harsh97mah <50320524+harsh97mah@users.noreply.github.com>
2019-07-11 10:29:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='newln2sql',
     version='0.2',
-    url='https://github.com/FerreroJeremy/ln2sql',
+    url='https://github.com/harsh97mah/newln2sql',
     license='GNU',
-    author='Jérémy Ferrero,Vikram Singh,Shashank Khare',
-    author_email='jeremy.ferrero@compilatio.net,mailvikram@email.com,shashank88@gmail.com',
+    author='Harshawardhan Mahajan',
+    author_email='humahajanrum007@gmail.com',
     description='Convert Natural Language to SQL queries',
     packages=find_packages(exclude=['tests']),
     long_description=open('README.md').read(),
